cce19ef4bb7d561ed9bcd04af679924e5d2e2237
Chris Roberts <chrisroberts.code@gmail.com>
2012-06-09 14:43:58 -0700

    Add support

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,8 @@ description      "Installs ruby"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
-supports 'ubuntu'
-supports 'debian'
+%w(oracle centos redhat scientific enterpriseenterprise fedora amazon ubuntu debian mint).each do |os|
+  supports os
+end
 
 depends 'build-essential'
