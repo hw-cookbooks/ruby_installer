e02af35b7e3ef7600f1d0c3977fd214ead1108de
Chris Roberts <chrisroberts.code@gmail.com>
2012-06-09 14:30:08 -0700

    Add remote location for REE source

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,3 +5,4 @@ default[:ruby_installer][:rubydev_package] = false
 default[:ruby_installer][:source_version] = nil
 default[:ruby_installer][:source_package_dependencies] = []
 default[:ruby_installer][:ree_url] = 'http://rubyenterpriseedition.googlecode.com/files/ruby-enterprise_1.8.7-2012.02_amd64_ubuntu10.04.deb'
+default[:ruby_installer][:ree_source_url] = 'http://rubyenterpriseedition.googlecode.com/files/ruby-enterprise-1.8.7-2012.02.tar.gz'
