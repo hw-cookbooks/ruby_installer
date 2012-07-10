b5ad92c058173e5f82a0ed577e10a58331b84683
Graham McIntire <gmcintire@gmail.com>
2012-07-10 16:53:18 -0500

    added source recipe

--- recipes/source.rb
+++ recipes/source.rb
@@ -1,4 +1,20 @@
-raise 'Not Implemented'
 Array(node[:ruby_installer][:source_package_dependencies]).each do |pkg|
   package pkg
+end
+
+remote_file "/tmp/ruby-#{node[:ruby_installer][:source_version]}.tar.gz" do
+  not_if "/usr/local/bin/ruby --version | grep -q '#{node[:ruby_installer][:source_version]}'"
+  source "http://ftp.ruby-lang.org/pub/ruby/#{node[:ruby_installer][:source_version][0..2]}/ruby-#{node[:ruby_installer][:source_version]}.tar.gz"
+  notifies :run, "bash[install_ruby]", :immediately
+end
+
+bash "install_ruby" do
+  user "root"
+  cwd "/tmp"
+  code <<-EOH
+    tar -zxf ruby-#{node[:ruby_installer][:source_version]}.tar.gz
+    (cd ruby-#{node[:ruby_installer][:source_version]}/ && ./configure && make -j#{node[:cpu][:total]+1} && make install)
+    rm -Rf /tmp/ruby-#{node[:ruby_installer][:source_version]}; rm -f /tmp/ruby-#{node[:ruby_installer][:source_version]}.tar.gz
+  EOH
+  action :nothing
 end
