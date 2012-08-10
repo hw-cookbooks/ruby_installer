71fb3bf9f37596cd2fb92ff2f86f0a644e37ad0d
Chris Roberts <chrisroberts.code@gmail.com>
2012-08-10 07:59:59 -0700

    Provide rubygems with source install

--- recipes/source.rb
+++ recipes/source.rb
@@ -2,19 +2,56 @@ Array(node[:ruby_installer][:source_package_dependencies]).each do |pkg|
   package pkg
 end
 
-remote_file "/tmp/ruby-#{node[:ruby_installer][:source_version]}.tar.gz" do
-  not_if "/usr/local/bin/ruby --version | grep -q '#{node[:ruby_installer][:source_version]}'"
+bash "install_ruby" do
+  cwd "/usr/src"
+  code <<-EOH
+    tar -zxf ruby-#{node[:ruby_installer][:source_version]}.tar.gz
+    cd ruby-#{node[:ruby_installer][:source_version]}/
+    autoconf
+    ./configure --prefix=#{node[:ruby_installer][:source_install_dir]} --disable-instal-doc --enable-shared
+    make -j#{node[:cpu][:total]+1}
+    make install
+  EOH
+  action :nothing
+end
+
+remote_file "/usr/src/ruby-#{node[:ruby_installer][:source_version]}.tar.gz" do
+  not_if do
+    begin
+      v = %{#{File.join(node[:ruby_installer][:source_install_dir], 'bin', 'ruby')} --version}.strip
+      v.split(' ')[1] == node[:ruby_installer][:source_version].sub('-', '')
+    rescue Errno::ENOENT
+      false
+    end
+  end
   source "http://ftp.ruby-lang.org/pub/ruby/#{node[:ruby_installer][:source_version][0..2]}/ruby-#{node[:ruby_installer][:source_version]}.tar.gz"
   notifies :run, "bash[install_ruby]", :immediately
 end
 
-bash "install_ruby" do
-  user "root"
-  cwd "/tmp"
-  code <<-EOH
-    tar -zxf ruby-#{node[:ruby_installer][:source_version]}.tar.gz
-    (cd ruby-#{node[:ruby_installer][:source_version]}/ && ./configure && make -j#{node[:cpu][:total]+1} && make install)
-    rm -Rf /tmp/ruby-#{node[:ruby_installer][:source_version]}; rm -f /tmp/ruby-#{node[:ruby_installer][:source_version]}.tar.gz
-  EOH
-  action :nothing
+rin_ver = Gem::Version.new(node[:ruby_installer][:source_version].match(/(\d+\.?){2,3}/).to_s)
+
+if(rin_ver < Gem::Version.new('1.9.0') || node[:ruby_installer][:source_rubygems_force])
+
+  bash 'install_rubygems' do
+    cwd "/usr/src"
+    command <<-EOH
+      tar -zxf rubygems-#{node[:ruby_installer][:source_rubygems_version]}.tgz
+      cd rubygems-#{node[:ruby_installer][:source_rubygems_version]}
+      #{File.join(node[:ruby_installer][:source_install_dir], 'bin', 'ruby')} setup.rb
+    EOH
+    action :nothing
+  end
+
+  remote_file "/usr/src/rubygems-#{node[:ruby_installer][:source_rubygems_version]}.tgz" do
+    source "http://production.cf.rubygems.org/rubygems/rubygems-#{node[:ruby_installer][:source_rubygems_version]}.tgz"
+    not_if do
+      begin
+        v = %x{#{File.join(node[:ruby_installer][:source_install_dir], 'bin', 'gem')} --version}.strip
+        v == node[:ruby_installer][:source_rubygems_version]
+      rescue Errno::ENOENT
+        false
+      end
+    end
+    notifies :run, 'bash[install_rubygems]', :immediately
+  end
 end
