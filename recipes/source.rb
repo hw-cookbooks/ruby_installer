b699a0edeb6a69719bc4a62b5fe29a55e4496c44
Chris Roberts <chrisroberts.code@gmail.com>
2012-08-10 08:33:58 -0700

    Set remote_file action to :create_if_missing

--- recipes/source.rb
+++ recipes/source.rb
@@ -1,3 +1,5 @@
+include_recipe 'build-essential'
+
 Array(node[:ruby_installer][:source_package_dependencies]).each do |pkg|
   package pkg
 end
@@ -25,33 +27,12 @@ remote_file "/usr/src/ruby-#{node[:ruby_installer][:source_version]}.tar.gz" do
     end
   end
   source "http://ftp.ruby-lang.org/pub/ruby/#{node[:ruby_installer][:source_version][0..2]}/ruby-#{node[:ruby_installer][:source_version]}.tar.gz"
+  action :create_if_missing
   notifies :run, "bash[install_ruby]", :immediately
 end
 
 rin_ver = Gem::Version.new(node[:ruby_installer][:source_version].match(/(\d+\.?){2,3}/).to_s)
 
 if(rin_ver < Gem::Version.new('1.9.0') || node[:ruby_installer][:source_rubygems_force])
-
-  bash 'install_rubygems' do
-    cwd "/usr/src"
-    command <<-EOH
-      tar -zxf rubygems-#{node[:ruby_installer][:source_rubygems_version]}.tgz
-      cd rubygems-#{node[:ruby_installer][:source_rubygems_version]}
-      #{File.join(node[:ruby_installer][:source_install_dir], 'bin', 'ruby')} setup.rb
-    EOH
-    action :nothing
-  end
-
-  remote_file "/usr/src/rubygems-#{node[:ruby_installer][:source_rubygems_version]}.tgz" do
-    source "http://production.cf.rubygems.org/rubygems/rubygems-#{node[:ruby_installer][:source_rubygems_version]}.tgz"
-    not_if do
-      begin
-        v = %x{#{File.join(node[:ruby_installer][:source_install_dir], 'bin', 'gem')} --version}.strip
-        v == node[:ruby_installer][:source_rubygems_version]
-      rescue Errno::ENOENT
-        false
-      end
-    end
-    notifies :run, 'bash[install_rubygems]', :immediately
-  end
+  include_recipe 'ruby_installer::source_rubygems'
 end
