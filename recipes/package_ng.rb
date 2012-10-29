11b9f67355f5c2e2cb7656fa759565c49b6ad197
Nathan L Smith <nlloyds@gmail.com>
2012-10-29 13:38:25 -0500

    update copyright

--- recipes/package_ng.rb
+++ recipes/package_ng.rb
@@ -2,7 +2,7 @@
 # Cookbook Name:: ruby_installer
 # Recipe:: package_ng
 #
-# Copyright 2008-2009, Opscode, Inc.
+# Copyright 2012, Cramer Development, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
