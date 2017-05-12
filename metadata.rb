de13d389a048cc7f1ab7af5a5382a33fd54f6e69
Erik Kranzusch <erikk@spindance.com>
2017-05-12 14:45:24 -0400

    Fix chef13 issues

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,12 @@
-name             "ruby_installer"
-maintainer       "Chris Roberts"
-maintainer_email "chrisroberts.code@gmail.com"
-license          "Apache 2.0"
-description      "Installs ruby"
+name             'ruby_installer'
+maintainer       'Chris Roberts'
+maintainer_email 'chrisroberts.code@gmail.com'
+license          'Apache 2.0'
+description      'Installs ruby'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.3"
+version          '0.2.0'
+issues_url 'https://github.com/chef-cookbooks/something/issues' if respond_to?(:issues_url)
+source_url 'https://github.com/chef-cookbooks/something' if respond_to?(:source_url)
 
 %w(oracle centos redhat scientific enterpriseenterprise fedora amazon ubuntu debian mint).each do |os|
   supports os
--- recipes/ree.rb
+++ recipes/ree.rb
@@ -1,22 +1,22 @@
 
-case node.platform_family
+case node['platform_family']
 when 'debian'
-  remote_file File.join('/', 'tmp', File.basename(node[:ruby_installer][:ree_url])) do
-    source node[:ruby_installer][:ree_url]
-    not_if{ ::File.exists?(File.join('/', 'tmp', File.basename(node[:ruby_installer][:ree_url]))) }
+  remote_file File.join('/', 'tmp', File.basename(node['ruby_installer']['ree_url'])) do
+    source node['ruby_installer']['ree_url']
+    not_if { ::File.exist?(File.join('/', 'tmp', File.basename(node['ruby_installer']['ree_url']))) }
   end
-  bash "Install REE deb" do
-    code "dpkg -i #{File.join('/', 'tmp', File.basename(node[:ruby_installer][:ree_url]))}; apt-get -f install"
-    not_if{
+  bash 'Install REE deb' do
+    code "dpkg -i #{File.join('/', 'tmp', File.basename(node['ruby_installer']['ree_url']))}; apt-get -f install"
+    not_if do
       begin
-        v = %x{ruby -v}.to_s.split(' ')
+        v = `ruby -v`.to_s.split(' ')
       rescue Errno::ENOENT
         v = []
       end
       version = "#{v[1]}-#{v.last}"
-      version == node[:ruby_installer][:ree_url].scan(/_(\d+[^_]+)_/).first.first
-    }
-    notifies :reload, resources(:ohai => 'ruby'), :immediately
+      version == node['ruby_installer']['ree_url'].scan(/_(\d+[^_]+)_/).first.first
+    end
+    notifies :reload, 'ohai[ruby]', :immediately
   end
 when 'fedora', 'rhel'
   include_recipe 'build-essential'
@@ -24,39 +24,39 @@ when 'fedora', 'rhel'
     package pkg
   end
 
-  remote_file "/usr/src/#{File.basename(node[:ruby_installer][:ree_source_url])}" do
-    source node[:ruby_installer][:ree_source_url]
+  remote_file "/usr/src/#{File.basename(node['ruby_installer']['ree_source_url'])}" do
+    source node['ruby_installer']['ree_source_url']
     not_if do
-      File.exists?("/usr/src/#{File.basename(node[:ruby_installer][:ree_source_url])}")
+      File.exist?("/usr/src/#{File.basename(node['ruby_installer']['ree_source_url'])}")
     end
   end
 
-   execute 'Extract REE' do
+  execute 'Extract REE' do
     cwd '/usr/src'
-    command "tar xvzf #{File.basename(node[:ruby_installer][:ree_source_url])}"
+    command "tar xvzf #{File.basename(node['ruby_installer']['ree_source_url'])}"
     not_if do
       begin
-        v = %x{ruby -v}.to_s.split(' ')
+        v = `ruby -v`.to_s.split(' ')
       rescue Errno::ENOENT
         v = []
       end
       version = "#{v[1]}-#{v.last}"
-      !File.exists?("/usr/src/#{File.basename(node[:ruby_installer][:ree_source_url])}") ||
-      version == node[:ruby_installer][:ree_url].scan(/_(\d+[^_]+)_/).first.first
+      !File.exist?("/usr/src/#{File.basename(node['ruby_installer']['ree_source_url'])}") ||
+        version == node['ruby_installer']['ree_url'].scan(/_(\d+[^_]+)_/).first.first
     end
   end
 
   execute 'Install REE' do
-    cwd "/usr/src/#{File.basename(node[:ruby_installer][:ree_source_url]).sub('.tar.gz', '')}"
+    cwd "/usr/src/#{File.basename(node['ruby_installer']['ree_source_url']).sub('.tar.gz', '')}"
     command './installer --auto=/usr/local/bin'
     action :nothing
-    subscribes :run, resources(:execute => 'Extract REE')
-    notifies :reload, resources(:ohai => 'ruby'), :immediately
+    subscribes :run, 'execute[Extract REE]'
+    notifies :reload, 'ohai[ruby]', :immediately
   end
 
   file '/etc/profile.d/ree_path' do
-    content "export PATH=/usr/local/bin:$PATH"
-    mode 0755
+    content 'export PATH=/usr/local/bin:$PATH'
+    mode '0755'
   end
 else
   raise 'Currently unsupported platform'
