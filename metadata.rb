24bde0f90e2a8252b04f8d92695499816bd77942
Erik Kranzusch <erikk@spindance.com>
2017-05-16 16:17:50 -0400

    Bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'chrisroberts.code@gmail.com'
 license          'Apache 2.0'
 description      'Installs ruby'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.0'
+version          '0.3.0'
 issues_url       'https://github.com/hw-cookbooks/ruby_installer/issues' if respond_to?(:issues_url)
 source_url       'https://github.com/hw-cookbooks/ruby_installer' if respond_to?(:source_url)
 chef_version     '>= 12.5.0'
