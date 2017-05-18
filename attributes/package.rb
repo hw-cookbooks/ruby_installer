6ad188b10e0159a0fe8a2ce74a43e0c4b05df4d7
Erik Kranzusch <erikk@spindance.com>
2017-05-18 09:37:53 -0400

    Fix headers, allow for nil package types

--- attributes/package.rb
+++ attributes/package.rb
@@ -1,4 +1,21 @@
-# Package attributes
+#
+# Cookbook Name:: ruby_installer
+# Attributes:: package
+#
+# Copyright 2012-2014, Chris Roberts <chrisroberts.code@gmail.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
 default['ruby_installer']['package_name'] = value_for_platform_family(
   'debian' => 'ruby-full',
@@ -10,5 +27,5 @@ default['ruby_installer']['rubydev_package'] = value_for_platform_family(
 )
 default['ruby_installer']['rubygem_package'] = value_for_platform_family(
   'debian' => 'rubygems-integration',
-  'default' => 'rubygems'
+  'default' => nil # 'rubygems'
 )
--- attributes/ree.rb
+++ attributes/ree.rb
@@ -1,4 +1,21 @@
-# REE attributes
+#
+# Cookbook Name:: ruby_installer
+# Attributes:: ree
+#
+# Copyright 2012-2014, Chris Roberts <chrisroberts.code@gmail.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
 default['ruby_installer']['ree_url'] = 'https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/rubyenterpriseedition/ruby-enterprise_1.8.7-2012.02_amd64_ubuntu10.04.deb'
 default['ruby_installer']['ree_source_url'] = 'https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/rubyenterpriseedition/ruby-enterprise-1.8.7-2012.02.tar.gz'
